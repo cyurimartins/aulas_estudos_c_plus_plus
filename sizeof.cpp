04a126fc180cdf1837751d6944f857f99ef2b643
Yuri Martins <cyuri.martins@gmail.com>
2017-08-26 17:54:11 -0300

    Update sizeof.cpp

--- sizeof.cpp
+++ sizeof.cpp
@@ -6,7 +6,9 @@ int main(int argc, char** argv) {
 	cout <<" --- Tamanho das variaveis --- \n"<<endl;
 	cout << "Tamanho de int: "<<sizeof(int)<<" bytes."<<endl;
 	cout << "Tamanho de short int: "<<sizeof(short)<<" bytes."<<endl;
+	cout << "Tamanho de long int: "<<sizeof(long)<<" bytes."<<endl;
 	cout << "Tamanho de float: "<<sizeof(float)<<" bytes."<<endl;
+	cout << "Tamanho de double: "<<sizeof(double)<<" bytes."<<endl;
 	cout << "Tamanho de bool: "<<sizeof(bool)<<" bytes."<<endl;
 	cout << "Tamanho de char: "<<sizeof(char)<<" bytes."<<endl;
 	
