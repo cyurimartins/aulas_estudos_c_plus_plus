8c6753bddc38ce240edf0b8c547b22b8ba284fd6
Yuri Martins <cyuri.martins@gmail.com>
2017-11-08 13:45:20 -0200

    Create funcaoRand.cpp

---
+++ funcaoRand.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+    int i;
+
+    for (i=0; i<10; i++){
+
+        cout << (rand() %11) <<endl;
+    }
+
+
+
+    return 0;
+}
